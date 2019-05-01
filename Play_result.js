c0b551251e9a7a13f0e4d2e3e598e4bce66a80a6
Jason_0607 <jasonsiauw90712223@gmail.com>
2019-05-02 02:45:35 +0800

    Add files via upload

---
+++ Play_result.js
@@ -0,0 +1,60 @@
+
+var fbhtml_url="https://snapcodexyz.github.io/index.html";
+var play_again = "https://snapcodexyz.github.io/game.html";
+setTimeout(function(){run()},1000);  // °õ¦æfunction
+
+function run(){
+	img_move1();
+	setTimeout(function(){img_move2()},1200);
+	setTimeout(function(){img_move3()},3000);
+	setTimeout(function(){img_move4()},3500);
+	setTimeout(function(){img_move5()},5000);
+	setTimeout(function(){img_move6()},6000);
+	setTimeout(function(){img_play_button()},7000);
+}
+
+
+
+function img_move1(){
+	$(".pic_top").animate({top:'-5%'},2200);
+}
+function img_move2(){
+	$(".pic_background").animate({top:'-70%'},1700);
+}
+function img_move3(){
+	$(".pic_background").animate({top:'-30%'},800);
+}
+function img_move4(){
+	$(".pic_background").animate({top:'-1%'},800);
+}
+function img_move5(){
+	$(".pic_bar").animate({top:'27px'},1100);
+}
+function img_move6(){
+	$(".pic_top").animate({top:'-50%'},2200);
+}
+
+function img_play_button(){
+	$(".play_button").fadeIn(0);
+}
+function play_video(){
+	$(".video").fadeIn(0);
+	//video_construction();
+}
+function close_video(){
+	$(".video").remove();
+	$(".play_button").remove();	
+}
+
+function video_construction(){
+	$(".background2").fadeIn(0);
+
+	
+}
+function close_construction(){
+	$(".background2").remove();
+	$(".play_button").remove();	
+}
+
+
+
